8f36986b67decf767ba4a23e8c4a33494205628b
Alexander Nazarov <nazavrik@gmail.com>
2016-01-31 16:40:17 +0300

    add product view controller

---
+++ evegano-ios/AddProductViewController.swift
@@ -0,0 +1,36 @@
+//
+//  AddProductViewController.swift
+//  evegano-ios
+//
+//  Created by alexander on 31.01.16.
+//  Copyright © 2016 nazavrik. All rights reserved.
+//
+
+import UIKit
+
+class AddProductViewController: UIViewController {
+
+    @IBOutlet weak var tempLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
